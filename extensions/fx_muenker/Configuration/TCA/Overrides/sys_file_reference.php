bfed51e904d158c271465e77ec881518a9165e41
FailXSiegen <info@failx.de>
2024-01-17 22:27:10 +0100

    add slide option and rounded-circle for images

---
+++ extensions/fx_muenker/Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
+
+call_user_func(
+    function ($extKey, $table) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, [
+            'image_style' => [
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'items' => [
+                        0 => [
+                            0 => 'Ohne Modifikation',
+                            1 => 'no-mod',
+                        ],
+                        1 => [
+                            0 => 'Graustufen',
+                            1 => 'grayscale',
+                        ],
+                        2 => [
+                            0 => 'Rund',
+                            1 => 'rounded-circle',
+                        ]
+                    ],
+                    'maxitems' => '1',
+                ],
+                'exclude' => '1',
+                'label' => 'Bild-Stil'
+            ],
+        ]);
+
+        $GLOBALS['TCA']['sys_file_reference']['palettes']['imageoverlayPalette']['showitem'] = '
+            title,alternative,
+            --linebreak--,
+            image_style,
+            --linebreak--,
+            description, link,
+            --linebreak--,
+            crop
+            ';
+    },
+    'fx_muenker',
+    'sys_file_reference'
+);
---
+++ extensions/fx_muenker/ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Table structure for table 'sys_file_reference'
+#
+CREATE TABLE sys_file_reference (
+    image_style varchar(20) DEFAULT '' NOT NULL
+);
